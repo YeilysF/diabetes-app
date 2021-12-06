68b0f6f5b52e60668798f2d4fa236ab387eb7591
hayronalvarez <halva023@fiu.edu>
2021-12-06 16:57:36 -0500

    search_added

--- health-tracker/screens/Home/HomeScreen.js
+++ health-tracker/screens/Home/HomeScreen.js
@@ -14,9 +14,58 @@ import Icon from "react-native-vector-icons/FontAwesome";
 
 const HomeScreen = (props) => {
     const { colors } = useTheme();
-
+    const searched = true; 
     const [searchQuery, setSearchQuery] = React.useState('');
-    const onChangeSearch = query => setSearchQuery(query);
+    const onChangeSearch = query => {
+      const medications = 'medications'
+      const glucose     = 'glucose'
+      const insulin     = 'insulin'
+      const bloodP      = 'blood pressure'
+      const nutrition   = 'nutrition'
+      const exercise    = 'exercise'
+      const empty       = ''
+      setSearchQuery(query);
+      if(empty.startsWith(query.toLowerCase())){
+        setItems([
+          { name: 'MEDICATIONS', link: require('../../assets/home_images/2.png'), color1: '#87cefa', color2: '#6495ed', width: 140, height: 70, screen: "Medications"},
+          { name: 'GLUCOSE', link: require('../../assets/home_images/1.png'), color1: '#f08080', color2: '#cd5c5c', width: 120, height: 70, screen: "Glucose"},
+          { name: 'INSULIN', link: require('../../assets/home_images/3.png'), color1: '#20b2aa', color2: '#48d1cc', width: 140, height: 70, screen: "Insulin"},
+          { name: 'BLOOD PRESSURE', link: require('../../assets/home_images/6.png'), color1: '#4682b4', color2: '#1e90ff', width: 140, height: 70, screen: "Blood Pressure"},
+          { name: 'NUTRITION', link: require('../../assets/home_images/7.png'), color1: '#daa520', color2: '#ffd700', width: 150, height: 70, screen: "Nutrition"}, 
+          { name: 'EXERCISE', link: require('../../assets/home_images/8.png'), color1: '#b0c4de', color2: '#4682b4', width: 150, height: 70, screen: "Exercise"},
+        ])
+      }
+      else if(medications.startsWith(query.toLowerCase())){
+        setItems([
+          { name: 'MEDICATIONS', link: require('../../assets/home_images/2.png'), color1: '#87cefa', color2: '#6495ed', width: 140, height: 70, screen: "Medications"}])
+      }else if (glucose.startsWith(query.toLowerCase())){
+        setItems([
+          { name: 'GLUCOSE', link: require('../../assets/home_images/1.png'), color1: '#f08080', color2: '#cd5c5c', width: 120, height: 70, screen: "Glucose"}])
+      }else if (insulin.startsWith(query.toLowerCase())){
+        setItems([
+          { name: 'INSULIN', link: require('../../assets/home_images/3.png'), color1: '#20b2aa', color2: '#48d1cc', width: 140, height: 70, screen: "Insulin"}])
+      }else if (bloodP.startsWith(query.toLowerCase())){
+        setItems([
+          { name: 'BLOOD PRESSURE', link: require('../../assets/home_images/6.png'), color1: '#4682b4', color2: '#1e90ff', width: 140, height: 70, screen: "Blood Pressure"}])
+      }else if (nutrition.startsWith(query.toLowerCase())){
+        setItems([
+          { name: 'NUTRITION', link: require('../../assets/home_images/7.png'), color1: '#daa520', color2: '#ffd700', width: 150, height: 70, screen: "Nutrition"}])
+      }else if (exercise.startsWith(query.toLowerCase())){
+        setItems([
+          { name: 'EXERCISE', link: require('../../assets/home_images/8.png'), color1: '#b0c4de', color2: '#4682b4', width: 150, height: 70, screen: "Exercise"}])
+      }else{
+        setItems([
+          { name: 'MEDICATIONS', link: require('../../assets/home_images/2.png'), color1: '#87cefa', color2: '#6495ed', width: 140, height: 70, screen: "Medications"},
+          { name: 'GLUCOSE', link: require('../../assets/home_images/1.png'), color1: '#f08080', color2: '#cd5c5c', width: 120, height: 70, screen: "Glucose"},
+          { name: 'INSULIN', link: require('../../assets/home_images/3.png'), color1: '#20b2aa', color2: '#48d1cc', width: 140, height: 70, screen: "Insulin"},
+          { name: 'BLOOD PRESSURE', link: require('../../assets/home_images/6.png'), color1: '#4682b4', color2: '#1e90ff', width: 140, height: 70, screen: "Blood Pressure"},
+          { name: 'NUTRITION', link: require('../../assets/home_images/7.png'), color1: '#daa520', color2: '#ffd700', width: 150, height: 70, screen: "Nutrition"}, 
+          { name: 'EXERCISE', link: require('../../assets/home_images/8.png'), color1: '#b0c4de', color2: '#4682b4', width: 150, height: 70, screen: "Exercise"},
+        ])
+      }
+      
+    }
+
 
     const [items, setItems] = React.useState([
       { name: 'MEDICATIONS', link: require('../../assets/home_images/2.png'), color1: '#87cefa', color2: '#6495ed', width: 140, height: 70, screen: "Medications"},
@@ -57,7 +106,7 @@ const HomeScreen = (props) => {
                     <TouchableOpacity
                     onPress={()=>props.navigation.navigate(item.screen)}
                     >
-                      <LinearGradient colors={[item.color1, item.color2]} style={[styles.itemContainer]} start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }}>
+                      <LinearGradient colors={[item.color1, item.color2]} style={[styles.itemContainer]} start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} >
                       <Text style={{marginBottom: 10}}>
                         <Image source={(item.link)} style={{width: item.width, height: item.height }}/>
                       </Text>
@@ -122,8 +171,8 @@ const styles = StyleSheet.create({
     //padding: 5,
     height: height * 0.19,
     width: height * 0.21,
-    //borderColor: '#3498db',
-    //borderWidth: 2,
+    //borderColor: '#FFD700',
+    //borderWidth: 10,
   },
   itemName: {
     fontSize: 17,
